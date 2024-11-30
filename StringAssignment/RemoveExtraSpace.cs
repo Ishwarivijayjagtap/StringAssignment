4605713ab4375f4a1ef8031451bdf75dea6ee385
Ishwarivijayjagtap <ishwarijagtap20701@gmail.com>
2024-11-30 19:26:09 +0530

    changes

--- StringAssignment/RemoveExtraSpace.cs
+++ StringAssignment/RemoveExtraSpace.cs
@@ -8,18 +8,18 @@ namespace StringAssignment
 {
     internal class RemoveExtraSpace
     {
-        //static void Main(string[] args)
-        //{
-        //    string input = "  Hello   World  ";
-        //    string result = "";
+        static void Main(string[] args)
+        {
+            string input = "  Hello   World  ";
+            string result = "";
 
-        //    for (int i = 0; i < input.Length; i++)
-        //    {
-        //        if (!(i > 0 && input[i] == ' ' && input[i - 1] == ' '))
-        //            result += input[i];
-        //    }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!(i > 0 && input[i] == ' ' && input[i - 1] == ' '))
+                    result += input[i];
+            }
 
-        //    Console.WriteLine(result.Trim());
-      //  }
+            Console.WriteLine(result.Trim());
+        }
     }
 }
